8b0d2581a8804c245741a7e43344de3e2979e86f
Vitalii Levda <vitalii.levda@tul.cz>
2025-09-05 14:20:58 +0300

    DiscountStrategy added

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.*;
+
+public class DiscountStrategy {
+    DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        return switch (specialEvent) {
+            case "Birthday" -> new BirthdayDiscountService();
+            case "Black Friday" -> new BlackFridayDiscountService();
+            case "New Year" -> new NewYearDiscountService();
+            default -> new DefaultDiscountService();
+        };
+    }
+}
