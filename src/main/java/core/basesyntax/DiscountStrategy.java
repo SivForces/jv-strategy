2faecb3a9c8d4606a4671c8518fe9976108a3e65
Vitalii Levda <vitalii.levda@tul.cz>
2025-09-05 14:28:14 +0300

    import changes

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 
-import core.basesyntax.strategy.*;
+import core.basesyntax.strategy.BirthdayDiscountService;
+import core.basesyntax.strategy.BlackFridayDiscountService;
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.NewYearDiscountService;
+import core.basesyntax.strategy.DefaultDiscountService;
+
 
 public class DiscountStrategy {
     DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
